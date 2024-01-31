c24b368a079565058dd10172e079acd3b3b7e4e0
lmy9237 <110085219+lmy9237@users.noreply.github.com>
2024-01-31 18:09:25 +0900

    충돌 해결

--- lastAirbnb2/src/myAction/Main3Servlet.java
+++ lastAirbnb2/src/myAction/Main3Servlet.java
@@ -3,10 +3,7 @@ package myAction;
 import java.io.IOException;
 import java.util.ArrayList;
 
-<<<<<<< Updated upstream
 import javax.servlet.RequestDispatcher;
-=======
->>>>>>> Stashed changes
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,25 +20,19 @@ import myVo.RoomInfoVo;
 import myVo.RoomVo;
 import myVo.StayVo;
 
-/**
- * Servlet implementation class Main3Servlet
- */
+
 @WebServlet("/Main3")
 public class Main3Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-<<<<<<< Updated upstream
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	      Main3Dao dao3 = new Main3Dao();
-	      
-	      // main2.jsp占쎈쐻占쎈윥占쎈군占쎈쐻占쎈윞占쎈쭓 占쎈쐻占쎈윞占쎈쭆占쎈눇�댆�슜爾쏉옙�쐻�뜝占� 占쎈쐻占쎈윥占쎈묍占쎈쐻占쎈윥�댆六웓x,占쎈쐻占쎈윪繹먮뛼�쐻占쎈윥獒뺤쉩�쐻占쎈윥獄��릥dx 占쎈쎗占쎈즵�뙴袁ㅻ뎨�뜝占�
-	      int roomIdx = Integer.parseInt(request.getParameter("roomIdx"));
+		 Main3Dao dao3 = new Main3Dao();
+
+		 int roomIdx = Integer.parseInt(request.getParameter("roomIdx"));
 	      RoomVo roomvo = dao3.getRoom(roomIdx);
 	      int hostidx = roomvo.getUser_idx();
 	      
 	      
-	      // �뜝�럥���뜝�럥�떁nfo 占쎈쐻占쎈윪占쎄껑�뜝�럥�걫�뜝�럥堉э옙逾녑뜝占�
 	      String str = roomvo.getRoom_info_idx(); //1,2,3
 	      String[] str2 = str.split(",");
 	      
@@ -51,31 +42,13 @@ public class Main3Servlet extends HttpServlet {
 	                                                
 	      int countReview = dao3.getCountReview(roomIdx);
 	      RoomImageVo imagevo = dao3.getRoomImage(roomIdx);
-=======
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Main3Dao dao3 = new Main3Dao();
-		 
-		int roomIdx = Integer.parseInt(request.getParameter("roomIdx"));
-	    RoomVo roomvo = dao3.getRoom(roomIdx);
-	    int hostidx = roomvo.getUser_idx(); 
-		
-	    String str = roomvo.getRoom_info_idx(); //1,2,3
-	    String[] str2 = str.split(",");
-	    String conv = roomvo.getConvenient_idx();
-	    
-	    int countReview = dao3.getCountReview(roomIdx);
-	    RoomImageVo imagevo = dao3.getRoomImage(roomIdx);
->>>>>>> Stashed changes
 	      UserInfoVo uservo = dao3.getUserInfo(hostidx);
 	      RoomExplainVo explainvo = dao3.getRoomExplain(roomIdx);
 	      ArrayList<RoomInfoVo> roominfovo = dao3.getRoomInfo(roomIdx,str2);
 	      ArrayList<ConvenientVo> convenientvo = dao3.getConvenientV2(conv);
 	      ArrayList<ReviewVo> reviewvo = dao3.getReview(roomIdx);
 	      ArrayList<StayVo> stayvo = dao3.getStay(roomIdx);
-<<<<<<< Updated upstream
 	      
-=======
->>>>>>> Stashed changes
 
 	      
 	      request.setAttribute("roomvo",roomvo);
@@ -86,28 +59,21 @@ public class Main3Servlet extends HttpServlet {
 	      request.setAttribute("roominfovo",roominfovo);
 	      request.setAttribute("reviewvo",reviewvo);
 	      request.setAttribute("convenientvo",convenientvo);
-<<<<<<< Updated upstream
 	      request.setAttribute("stayvo",stayvo);
-	      for(StayVo vo : stayvo) {
-	    	  if(vo.getStay_img().contains("svg")){
-	    		  System.out.println(vo.getStay_img());
-	    	  }else {
-	    		  System.out.println(vo.getStay_img());
-	    	  }
-	      }
-	      
+//	      for(StayVo vo : stayvo) {
+//	    	  if(vo.getStay_img().contains("svg")){
+//	    		  System.out.println(vo.getStay_img());
+//	    	  }else {
+//	    		  System.out.println(vo.getStay_img());
+//	    	  }
+//	      }
+//	      
 	      
 	      
 	      RequestDispatcher rd = request.getRequestDispatcher("main3.jsp");
 	      rd.forward(request, response);
 	   }
-	
-	
-=======
-	      request.setAttribute("stayvo",stayvo);	    
-	}
 
->>>>>>> Stashed changes
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
